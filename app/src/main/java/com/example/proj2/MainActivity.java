676d475c687eb84b3e3c78803d12d6fcd6b09494
SudhanshuSahil <51629267+SudhanshuSahil@users.noreply.github.com>
2019-10-28 19:12:42 +0530

    nav bar

--- app/src/main/java/com/example/proj2/MainActivity.java
+++ app/src/main/java/com/example/proj2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.proj2;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +24,32 @@ public class MainActivity extends AppCompatActivity {
         dbHelper.cc();
         dbHelper.checker();
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         Button btn1 = (Button) findViewById(R.id.button1);
 
         btn1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) { Log.d("MyApp","I am here");
+            public void onClick(View v) {
+                Log.d("MyApp", "I am here");
                 Intent myIntent = new Intent(MainActivity.this, Main2Activity.class);
                 MainActivity.this.startActivity(myIntent);
             }
         });
 
-
-
-
     }
+
+
 }
